6ab15a1478ebc211dddefefcc084131d74dbef85
Nisal4822 <134356860+Nisal4822@users.noreply.github.com>
2023-08-01 13:15:21 +0530

    Add files via upload

---
+++ WEB github/Day 12 (11.07.2023)/Q1/labday12Q1.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Arithmetic Operations</title>
+</head>
+<body>
+<?php
+$x=10;
+$y=20;
+echo "Sum: ",($x+$y)."<br>";
+echo "Sub: ",($x-$y)."<br>";
+echo "Multi: ",($x*$y)."<br>";
+echo "Div: ",($x/$y)."<br>";
+?>
+</body>
+</html>
